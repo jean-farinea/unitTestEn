0ab43c181e6b2b9d8e038034fc635fe186628434
Jean Farinea <jeanfarinea@gmail.com>
2023-07-20 20:07:56 +0100

    Calculator class created with unit test for each operation

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Calculator {
+
+    public static int sum(int numberA, int numberB) {
+        return numberA + numberB;
+    }
+
+    public static int subtract(int numberA, int numberB) {
+        return numberA - numberB;
+    }
+
+    public static int multiply(int numberA, int numberB) {
+        return numberA * numberB;
+    }
+
+    public static int divide(int numberA, int numberB) {
+        return numberA / numberB;
+    }
+
+}
