03ab7334ab2d59c267c975fabba4aae2243c682f
Jean Farinea <jeanfarinea@gmail.com>
2023-08-03 12:41:55 +0100

    1.Re-organizing files to have 1 task per branch. 2.Re-naming methods to make more readable

--- src/main/java/lesson2/task4/AgeVerifier.java
+++ src/main/java/lesson2/task4/AgeVerifier.java
@@ -1,7 +1,7 @@
-package org.example;
+package lesson2.task4;
 
 public class AgeVerifier {
-    public static boolean verifyAgeGreaterOrEqual18(int age){
+    public static boolean isAdult(int age){
         if(age >= 18){
             return true;
         }else{
--- src/test/java/lesson2/task4/AgeVerifierTest.java
+++ src/test/java/lesson2/task4/AgeVerifierTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package lesson2.task4;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -6,16 +6,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AgeVerifierTest {
+
     @Test
-    void shouldVerifyAgeGreaterOrEqual18() {
+    void shouldVerifyIfIsAdult() {
         //given
         int numberA = 17;
         int numberB = 18;
         int numberC = 19;
         //when
-        boolean result1 = AgeVerifier.verifyAgeGreaterOrEqual18(numberA);
-        boolean result2 = AgeVerifier.verifyAgeGreaterOrEqual18(numberB);
-        boolean result3 = AgeVerifier.verifyAgeGreaterOrEqual18(numberC);
+        boolean result1 = AgeVerifier.isAdult(numberA);
+        boolean result2 = AgeVerifier.isAdult(numberB);
+        boolean result3 = AgeVerifier.isAdult(numberC);
         //then
         Assertions.assertFalse(result1);
         Assertions.assertTrue(result2);
