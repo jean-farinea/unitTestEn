503532e81721191ffbf88c7a1d9d1e49c19dbfe1
jean-farinea <jeanfarinea@gmail.com>
2023-08-06 18:11:26 +0100

    1. Adjusted formatting code 2. Separated the tests into two for each case whether is even or not

--- src/main/java/lesson2/task5/IsEvenOrOdd.java
+++ src/main/java/lesson2/task5/IsEvenOrOdd.java
@@ -2,8 +2,8 @@ package lesson2.task5;
 
 public class IsEvenOrOdd {
 
-    public static boolean isEven(int number){
-        if (number %2 == 0) {
+    public static boolean isEven(int number) {
+        if (number % 2 == 0) {
             return true;
         } else {
             return false;
--- src/test/java/lesson2/task5/IsEvenOrOddTest.java
+++ src/test/java/lesson2/task5/IsEvenOrOddTest.java
@@ -6,15 +6,22 @@ import org.junit.jupiter.api.Test;
 class IsEvenOrOddTest {
 
     @Test
-    void verifyIfNumberIsEvenOrOdd() {
+    void shouldVerifyIfNumberIsEven() {
         //given
         int numberA = 2;
-        int numberB = 3;
         //when
         boolean result1 = IsEvenOrOdd.isEven(numberA);
-        boolean result2 = IsEvenOrOdd.isEven(numberB);
         //then
         Assertions.assertTrue(result1);
-        Assertions.assertFalse(result2);
+    }
+
+    @Test
+    void shouldVerifyIfNumberIsNotEven() {
+        //given
+        int numberA = 3;
+        //when
+        boolean result1 = IsEvenOrOdd.isEven(numberA);
+        //then
+        Assertions.assertFalse(result1);
     }
 }
