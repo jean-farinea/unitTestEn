cb73373b2f114dde3a0173e6e7b326db15c2e1ad
Jean Farinea <jeanfarinea@gmail.com>
2023-08-16 22:10:52 +0100

    1.Animal class created 2.Lion class created extending animal

---
+++ src/main/java/lesson4/task5/Animal.java
@@ -0,0 +1,13 @@
+package lesson4.task5;
+
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/lesson4/task5/Lion.java
@@ -0,0 +1,9 @@
+package lesson4.task5;
+
+public class Lion extends Animal {
+
+    public Lion(String name) {
+        super(name);
+    }
+
+}
