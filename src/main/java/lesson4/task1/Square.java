c28404673bd66bd727fc22cd984e4664c128a36a
farinea <jeanfarinea@gmail.com>
2023-08-20 05:45:13 +0100

    1. Square class now inherit from rectangle class giving side/side as width/height to make sure getArea and getPerimeter will work proper, tests are giving expected results.

--- src/main/java/lesson4/task1/Square.java
+++ src/main/java/lesson4/task1/Square.java
@@ -1,21 +1,9 @@
 package lesson4.task1;
 
-public class Square extends Shape {
-    private double side;
+public class Square extends Rectangle {
 
     public Square(double side) {
-        this.side = side;
+        super(side, side);
     }
 
-    @Override
-    public double getArea() {
-        double area = side * side;
-        return area;
-    }
-
-    @Override
-    public double getPerimeter() {
-        double perimeter = side * 4;
-        return perimeter;
-    }
 }
