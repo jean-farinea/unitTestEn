3f51fbf28a46a7ad9c43f6e7a3342cbfa726062a
farinea <jeanfarinea@gmail.com>
2023-08-20 03:35:00 +0100

    1. Enhanced Calculator class created with separated classes for each operation

---
+++ src/main/java/lesson5/task3/Divide.java
@@ -0,0 +1,10 @@
+package lesson5.task3;
+
+public class Divide extends EnhancedCalculator {
+
+    @Override
+    public double calculate(double numberA, double numberB) {
+        return numberA / numberB;
+    }
+
+}
---
+++ src/main/java/lesson5/task3/EnhancedCalculator.java
@@ -0,0 +1,7 @@
+package lesson5.task3;
+
+public abstract class EnhancedCalculator {
+
+    public abstract double calculate(double numberA, double numberB);
+
+}
