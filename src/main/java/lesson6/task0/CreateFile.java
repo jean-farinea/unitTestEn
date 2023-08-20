1b4ac286f1a72502a2ea10a0b035baded35e8434
farinea <jeanfarinea@gmail.com>
2023-08-20 05:14:02 +0100

    1. FileUtils class created to be used for other tasks 2. CreateFile class created

---
+++ src/main/java/lesson6/task0/CreateFile.java
@@ -0,0 +1,8 @@
+package lesson6.task0;
+
+public class CreateFile {
+
+    public static void main(String[] args) {
+        FileUtils.writeToFile("src/main/resources/data.txt", "Course\nJava\nLesson 6\nFiles\nExceptions\nFiles\nEnd of file");
+    }
+}
