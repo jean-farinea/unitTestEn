9a1a430ad390b60d9471bb4f079814395f503807
farinea <jeanfarinea@gmail.com>
2023-08-24 01:12:30 +0100

    1. Created class with function to print every second line of a file.

---
+++ src/main/java/lesson6/task2/PrintEverySecondLine.java
@@ -0,0 +1,18 @@
+package lesson6.task2;
+
+import lesson6.task0.FileUtils;
+
+import java.io.IOException;
+
+public class PrintEverySecondLine {
+
+    public static void printEverySecondLine() throws IOException {
+        String filePath = "src/main/resources/data.txt";
+        String content = FileUtils.readFromFile(filePath);
+        String[] lines = content.split(System.lineSeparator());
+        for (int i = 1; i < lines.length; i += 2) {
+            System.out.println(lines[i]);
+        }
+    }
+
+}
