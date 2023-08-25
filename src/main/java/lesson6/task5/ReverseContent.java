fb682506836051924a0f337079bfbb10bc971776
farinea <jeanfarinea@gmail.com>
2023-08-25 06:41:46 +0100

    1. The filePath is added as a parameter for the method.

--- src/main/java/lesson6/task5/ReverseContent.java
+++ src/main/java/lesson6/task5/ReverseContent.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 public class ReverseContent {
 
-    public static void printReverseContent() throws IOException {
-        List<String> lines = Files.readAllLines(Path.of("src/main/resources/data.txt"));
+    public static void printReverseContent(String filePath) throws IOException {
+        List<String> lines = Files.readAllLines(Path.of(filePath));
         Collections.reverse(lines);
         for (String line : lines) {
             System.out.println(line);
