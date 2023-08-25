1076c9a56d78ccb1d57a0cb1217d3516600fcbee
farinea <jeanfarinea@gmail.com>
2023-08-25 05:20:19 +0100

    1. Bicycle class adjusted without implement Vehicle class.

--- src/main/java/lesson5/task5/Bicycle.java
+++ src/main/java/lesson5/task5/Bicycle.java
@@ -1,29 +1,9 @@
 package lesson5.task5;
 
-public class Bicycle extends Vehicle {
+public class Bicycle {
 
-    public Bicycle(boolean engineStarted) {
-        super(engineStarted);
+    public Bicycle() {
+
     }
 
-
-    @Override
-    protected void startEngine() {
-        throw new UnsupportedOperationException("Bicycle does not have engine");
-    }
-
-    @Override
-    protected void stopEngine() {
-        throw new UnsupportedOperationException("Bicycle does not have engine");
-    }
-
-    @Override
-    public boolean isEngineStarted() {
-        throw new UnsupportedOperationException("Bicycle does not have engine");
-    }
-
-    @Override
-    public void setEngineStarted(boolean engineStarted) {
-        throw new UnsupportedOperationException("Bicycle does not have engine");
-    }
 }
