ce4b7cfd5eec21f28f03702112e8c5982b5436ef
farinea <jeanfarinea@gmail.com>
2023-08-28 11:35:40 +0100

    1. Class name adjusted to TextConverter 2. Throwing null pointer exception at the method now. 3. Tests adjusted

--- src/main/java/lesson7/task2/ConvertToCapital.java
+++
@@ -1,9 +0,0 @@
-package lesson7.task2;
-
-public class ConvertToCapital {
-
-    public static String convertTextToCapital(String text) {
-        String convertedText = text.toUpperCase();
-        return convertedText;
-    }
-}
--- src/test/java/lesson7/task2/TextConverterTest.java
+++ src/test/java/lesson7/task2/TextConverterTest.java
@@ -7,14 +7,18 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ConvertToCapitalTest {
+class TextConverterTest {
 
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = {"first text", "second text", "third text"})
     void shouldCheckIfTextIsConvertedToCapital(String text) {
+        //given
         String expectedText = text.toUpperCase();
-        String convertedText = ConvertToCapital.convertTextToCapital(text);
+        //when
+        String convertedText = TextConverter.convertTextToCapital(text);
+        //then
         Assertions.assertEquals(convertedText, expectedText);
+        assertThrows(NullPointerException.class, () -> TextConverter.convertTextToCapital(null));
     }
 }
