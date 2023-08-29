52a322b2ab1999dd13cf117fdd12cb74aaf5ce8b
farinea <jeanfarinea@gmail.com>
2023-08-29 11:40:13 +0100

    1. Test class adjusted to positive and negative scenario

--- src/test/java/lesson7/task2/TextConverterTest.java
+++ src/test/java/lesson7/task2/TextConverterTest.java
@@ -1,6 +1,8 @@
 package lesson7.task2;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,7 +12,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class TextConverterTest {
 
     @ParameterizedTest
-    @NullAndEmptySource
     @ValueSource(strings = {"first text", "second text", "third text"})
     void shouldCheckIfTextIsConvertedToCapital(String text) {
         //given
@@ -19,6 +20,15 @@ class TextConverterTest {
         String convertedText = TextConverter.convertTextToCapital(text);
         //then
         Assertions.assertEquals(convertedText, expectedText);
-        assertThrows(NullPointerException.class, () -> TextConverter.convertTextToCapital(null));
+    }
+
+
+    @Test
+    @NullAndEmptySource
+    void shouldThrowExceptionWhenTextIsNull() {
+        //given
+        String nullText = null;
+        //then
+        assertThrows(NullPointerException.class, () -> TextConverter.convertTextToCapital(nullText));
     }
 }
