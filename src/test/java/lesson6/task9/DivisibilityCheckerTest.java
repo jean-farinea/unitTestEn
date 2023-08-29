ddd46adee32ca922f1fb033bd45833020137c347
farinea <jeanfarinea@gmail.com>
2023-08-29 11:34:01 +0100

    1. Test class adjusted to positive and negative scenario

--- src/test/java/lesson6/task9/DivisibilityCheckerTest.java
+++ src/test/java/lesson6/task9/DivisibilityCheckerTest.java
@@ -9,13 +9,17 @@ class DivisibilityCheckerTest {
     @Test
     void shouldCheckDivisibilityByThree() {
         //given
+        int[] values = {3, 6, 9, 12, 15};
+        //then
+        assertDoesNotThrow(() -> DivisibilityChecker.validate(values));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenNotDivisibleByThree() {
+        //given
         int[] values = {3, 6, 9, 12, 13};
-        int[] values2 = {3, 6, 9, 12, 15};
-        //when
-        DivisibilityChecker.validate(values);
         //then
         assertThrows(IllegalArgumentException.class, () -> DivisibilityChecker.validate(values));
-        assertDoesNotThrow(() -> DivisibilityChecker.validate(values2));
     }
 
 }
